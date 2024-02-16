30590125714f7e9be1a048e740b048e89306b66a
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-02-16 14:19:47 +0530

    Create allSettled.js

---
+++ Javascript/Promise API/allSettled.js
@@ -0,0 +1,23 @@
+const p1 = new Promise((resolve, reject)=>{
+    // setTimeout(()=>resolve("P1 sucess"),3000);
+    setTimeout(()=>reject("P1 fail"),3000);
+});
+
+const p2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve("P2 sucess"),1000);
+    // setTimeout(()=>reject("P2 fail"),1000);
+});
+
+const p3 = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve("P3 sucess"),2000);
+    // setTimeout(()=>reject("P3 fail"),2000);
+});
+
+
+Promise.allSettled([p1, p2, p3])
+.then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.error(err);
+});
